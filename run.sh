8090c35df605dce1e0453e49f8c5625a3f5f045b
dujun <jun.du@intel.com>
2022-12-14 08:06:41 +0000

    update 2dunet

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 python setup.py develop
 pip install h5py
 
-ln -s /home2/pytorch-broad-models/gpuoob/Unet/3dunet_datasets 3dunet_datasets 
+ln -s /home2/pytorch-broad-models/gpuoob/Unet/3dunet_datasets 3dunet_datasets
+ln -s /home2/pytorch-broad-models/gpuoob/Unet/2dunet_datasets 2dunet_datasets
 
 python pytorch3dunet/train.py --config resources/3DUnet_confocal_boundary/train_config.yml
