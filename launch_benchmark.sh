d0489bb8e72e3312d1b51d89cae890c7e001b309
jun.du@intel.com <jun.du@intel.com>
2024-03-19 22:22:44 -0700

    update PT run with multi instance

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -58,15 +58,17 @@ function generate_core {
             OOB_EXEC_HEADER+=" -C $(echo ${device_array[i]} |awk -F ';' '{print $1}') "
         elif [ "${device}" == "cuda" ];then
             OOB_EXEC_HEADER=" CUDA_VISIBLE_DEVICES=${device_array[i]} "
-	    if [[ "${mode_name}" == "realtime" ]];then
-	        addtion_options+=" --nv_fuser "
+            if [[ "${mode_name}" == "realtime" ]];then
+                addtion_options+=" --nv_fuser "
+            fi
+        elif [ "${device}" == "xpu" ];then
+            OOB_EXEC_HEADER=" ZE_AFFINITY_MASK=${i} "
 	    fi
-	fi
         printf " ${OOB_EXEC_HEADER} \
 	    python pytorch3dunet/train.py --config ${train_config} \
 	        --batch_size ${batch_size} --device ${device} \
-		--num_iter $num_iter --num_warmup $num_warmup \
-		--channels_last $channels_last --precision $precision \
+		    --num_iter $num_iter --num_warmup $num_warmup \
+		    --channels_last $channels_last --precision $precision \
                 ${addtion_options} \
         > ${log_file} 2>&1 &  \n" |tee -a ${excute_cmd_file}
         if [ "${numa_nodes_use}" == "0" ];then
